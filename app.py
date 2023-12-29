f707aea81b6c51499dd5fc40b38c0b3a9621520d
munal0803 <75110825+munal0803@users.noreply.github.com>
2023-12-29 16:05:00 +0000

    added dyamic data

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+JOBS = [
+  {"name":"Data Scientist", "location":"San Francisco", "salary":"$100,000"},
+   {"name":"Data Scientist", "location":"San Francisco", "salary":"$100,000"},
+   {"name":"Data Scientist", "location":"San Francisco", "salary":"$100,000"}
+]
+
 @app.route("/")
 def hello_world():
-  return "Hello world"
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
